1595897cfa7afdfad944a6df0b187a308da8488d
Colombine972 <decidela972@me.com>
2025-11-17 15:54:05 +0100

    add Rules.css

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -1,4 +1,11 @@
 function Rules() {
-	return <></>;
+	return (
+		<div className="rules-page">
+			<div className="parechment">
+				<h1>Règles du Jeu</h1>
+				<p>mon texte ici</p>
+			</div>
+		</div>
+	);
 }
 export default Rules;
