9f7ff7f919c4ca1d9fd4d662f29a287f3c56bb72
Colombine972 <decidela972@me.com>
2025-11-20 23:17:02 +0100

    modification marauderMap and rules.tsx

--- src/pages/Rules.tsx
+++ src/pages/Rules.tsx
@@ -5,14 +5,37 @@ import marauderMapGauche from "../assets/images/marauder-map-gauche.jpg";
 import parcheminVide from "../assets/images/parchemin-vide.webp";
 import "../styles/Rules.css";
 
-const fullText = `📜 Règles du jeu
-      
-Bienvenue jeune sorcier !
-Le but du jeu est simple : trouve le personnage mystère du jour !
+const fullText = `🪄 Règles du jeu – WIZDLE : Le défi des sorciers
 
-Toutes tes tentatives seront affichées et comparées avec le personnage secret…
+WIZDLE est un jeu de déduction basé sur l’univers d’Harry Potter.
 
-Bonne chance, et que la magie soit avec toi ✨`;
+Le but est de trouver le personnage mystère du jour.
+
+Un nouveau personnage est choisi chaque jour de manière aléatoire.
+
+Vous devez deviner en un minimum d’essais et de temps.
+
+Chaque personnage possède plusieurs caractéristiques à analyser : espèce, genre, sang, maison, statut de vie, cheveux.
+
+À chaque tentative, un tableau affiche votre réponse et les retours.
+
+🟩 Vert signifie que la caractéristique est correcte.
+
+🟥 Rouge signifie que la caractéristique est incorrecte.
+
+La recherche s’effectue dans une barre dédiée avec une liste de suggestions.
+
+Vous validez un personnage pour obtenir immédiatement les indices colorés.
+
+Vous ajustez ensuite vos suppositions en fonction des retours.
+
+Un bouton Indice apparaît après un certain nombre d’essais.
+
+L’indice révèle la première lettre du prénom mais réduit votre score final.
+
+Le score dépend du nombre de tentatives, du temps écoulé et de l’usage de l’indice.
+
+Des modes supplémentaires existent : partie du jour, partie de la veille, calendrier, et classement journalier.`;
 
 function Rules() {
 	const [showParchment, setShowParchment] = useState(false);
@@ -57,7 +80,7 @@ function Rules() {
 				clearInterval(interval);
 				setGameButton(true); // ton bouton apparaît après
 			}
-		}, 50); // vitesse d’écriture
+		}, 30); // vitesse d’écriture
 	}, [validOath]);
 
 	return (
@@ -101,7 +124,9 @@ function Rules() {
 							)}
 							{validOath && (
 								<div className="rules-container">
-									<div className="magic-text">{displayedText}</div>
+									<div className="rules-scroll">
+										<div className="magic-text">{displayedText}</div>
+									</div>
 									{gameButton && (
 										<button
 											type="button"
