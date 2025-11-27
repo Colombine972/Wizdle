029d9be1f0acdc99b09ef239accc03e3948d93d0
Thomas Rieu <rieu.thomas@hotmail.com>
2025-11-27 11:06:57 +0100

    css-fix

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,7 +1,7 @@
 import moment from "moment";
 import "../styles/score.css";
-import "../styles/scoreResponsive.css";
-import imgCoix from "../images/croix.svg";
+import imgCastle from "../images/carte-chateau.png";
+import imgCroix from "../images/croix.svg";
 import imgSablier from "../images/sablier.svg";
 import type { Character } from "../interfaces/interfaces";
 
@@ -35,58 +35,65 @@ function Score({
 
 	return (
 		<section id="popop-score">
-			<img
-				src={imgCoix}
-				alt="Une croix"
-				onClick={() => setScoreView(false)}
-				onKeyDown={(e) => {
-					if (e.key === "Enter" || e.key === "Spacebar" || e.key === " ")
-						setScoreView(false);
-				}}
-			/>
-			<article>
-				<h2>MAGISTRAL !</h2>
-				<p>{score} pts</p>
-				<p>pour {house}</p>
-				<div>
-					<img src={imgSablier} alt="Un sablier" />
-					<p>{displayTime}</p>
+			<div className="popup-container">
+				<img src={imgCastle} alt="Chateau" className="bg-img" />
+
+				<img
+					src={imgCroix}
+					alt="Une croix"
+					className="close-btn"
+					onClick={() => setScoreView(false)}
+					onKeyDown={(e) => {
+						if (e.key === "Enter" || e.key === "Spacebar" || e.key === " ")
+							setScoreView(false);
+					}}
+				/>
+				<div className="scroll-div">
+					<article>
+						<h2>MAGISTRAL !</h2>
+						<p>{score} pts</p>
+						<p>pour {house}</p>
+						<div>
+							<img src={imgSablier} alt="Un sablier" />
+							<p>{displayTime}</p>
+						</div>
+						<div>
+							<p>
+								Indice utilisé :<span>{usedClue ? "oui" : "non"}</span>
+							</p>
+							<p>
+								Tentatives :<span>{attemptCount}</span>
+							</p>
+						</div>
+					</article>
+					<article>
+						<p>Carte : {todayCharacter?.nom}</p>
+						<button type="button" aria-label="Une croix">
+							<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
+								<title>Icône croix</title>
+								<line
+									x1="50"
+									y1="20"
+									x2="50"
+									y2="80"
+									stroke="white"
+									strokeWidth="10"
+									strokeLinecap="round"
+								/>
+								<line
+									x1="20"
+									y1="50"
+									x2="80"
+									y2="50"
+									stroke="white"
+									strokeWidth="10"
+									strokeLinecap="round"
+								/>
+							</svg>
+						</button>
+					</article>
 				</div>
-				<div>
-					<p>
-						Indice utilisé :<span>{usedClue ? "oui" : "non"}</span>
-					</p>
-					<p>
-						Tentatives :<span>{attemptCount}</span>
-					</p>
-				</div>
-			</article>
-			<article>
-				<p>Carte : {todayCharacter?.nom}</p>
-				<button type="button" aria-label="Une croix">
-					<svg viewBox="0 0 100 100" xmlns="http://www.w3.org/2000/svg">
-						<title>Icône croix</title>
-						<line
-							x1="50"
-							y1="20"
-							x2="50"
-							y2="80"
-							stroke="white"
-							strokeWidth="10"
-							strokeLinecap="round"
-						/>
-						<line
-							x1="20"
-							y1="50"
-							x2="80"
-							y2="50"
-							stroke="white"
-							strokeWidth="10"
-							strokeLinecap="round"
-						/>
-					</svg>
-				</button>
-			</article>
+			</div>
 		</section>
 	);
 }
